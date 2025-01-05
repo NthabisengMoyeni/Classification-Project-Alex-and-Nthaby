3cd09e272f7e084a41262525581e4671e952262d
NthabisengMoyeni <nthaby.thateng@gmail.com>
2025-01-05 15:04:46 +0200

    Update Classification_app.py

--- Classification_app.py
+++ Classification_app.py
@@ -54,7 +54,7 @@
 # Import necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
